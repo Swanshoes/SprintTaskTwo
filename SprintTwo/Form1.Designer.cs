c3d0fb0cbc0a89e5e210fc204357e6afadeaaf77
Swanshoes <booth.nathan1@gmail.com>
2025-05-21 13:52:57 +0800

    Add files via upload

---
+++ SprintTwo/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace SprintOne
+{
+    partial class NeutrinoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataInput = new System.Windows.Forms.TextBox();
+            this.BTNAddData = new System.Windows.Forms.Button();
+            this.DataEditInput = new System.Windows.Forms.TextBox();
+            this.BTNEditData = new System.Windows.Forms.Button();
+            this.BTNSearch = new System.Windows.Forms.Button();
+            this.BTNSortAsc = new System.Windows.Forms.Button();
+            this.BTNSortDesc = new System.Windows.Forms.Button();
+            this.DataListBox = new System.Windows.Forms.ListBox();
+            this.DataSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GenerateRandomData = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DataInput
+            // 
+            this.DataInput.Location = new System.Drawing.Point(132, 36);
+            this.DataInput.Name = "DataInput";
+            this.DataInput.Size = new System.Drawing.Size(197, 20);
+            this.DataInput.TabIndex = 0;
+            // 
+            // BTNAddData
+            // 
+            this.BTNAddData.Location = new System.Drawing.Point(24, 33);
+            this.BTNAddData.Name = "BTNAddData";
+            this.BTNAddData.Size = new System.Drawing.Size(102, 23);
+            this.BTNAddData.TabIndex = 1;
+            this.BTNAddData.Text = "Add Data Point";
+            this.BTNAddData.UseVisualStyleBackColor = true;
+            this.BTNAddData.Click += new System.EventHandler(this.BTNAddData_Click);
+            // 
+            // DataEditInput
+            // 
+            this.DataEditInput.Location = new System.Drawing.Point(132, 74);
+            this.DataEditInput.Name = "DataEditInput";
+            this.DataEditInput.Size = new System.Drawing.Size(197, 20);
+            this.DataEditInput.TabIndex = 2;
+            // 
+            // BTNEditData
+            // 
+            this.BTNEditData.Location = new System.Drawing.Point(24, 72);
+            this.BTNEditData.Name = "BTNEditData";
+            this.BTNEditData.Size = new System.Drawing.Size(102, 23);
+            this.BTNEditData.TabIndex = 3;
+            this.BTNEditData.Text = "Edit Data Point";
+            this.BTNEditData.UseVisualStyleBackColor = true;
+            this.BTNEditData.Click += new System.EventHandler(this.BTNEditData_Click);
+            // 
+            // BTNSearch
+            // 
+            this.BTNSearch.Location = new System.Drawing.Point(24, 115);
+            this.BTNSearch.Name = "BTNSearch";
+            this.BTNSearch.Size = new System.Drawing.Size(102, 27);
+            this.BTNSearch.TabIndex = 4;
+            this.BTNSearch.Text = "Search Data";
+            this.BTNSearch.UseVisualStyleBackColor = true;
+            this.BTNSearch.Click += new System.EventHandler(this.BTNSearch_Click);
+            // 
+            // BTNSortAsc
+            // 
+            this.BTNSortAsc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.BTNSortAsc.Location = new System.Drawing.Point(118, 178);
+            this.BTNSortAsc.Name = "BTNSortAsc";
+            this.BTNSortAsc.Size = new System.Drawing.Size(117, 42);
+            this.BTNSortAsc.TabIndex = 5;
+            this.BTNSortAsc.Text = "Sort Data Asc";
+            this.BTNSortAsc.UseVisualStyleBackColor = false;
+            this.BTNSortAsc.Click += new System.EventHandler(this.BTNSortAsc_Click);
+            // 
+            // BTNSortDesc
+            // 
+            this.BTNSortDesc.BackColor = System.Drawing.Color.Lime;
+            this.BTNSortDesc.Location = new System.Drawing.Point(118, 237);
+            this.BTNSortDesc.Name = "BTNSortDesc";
+            this.BTNSortDesc.Size = new System.Drawing.Size(117, 42);
+            this.BTNSortDesc.TabIndex = 6;
+            this.BTNSortDesc.Text = "Sort Data Desc";
+            this.BTNSortDesc.UseVisualStyleBackColor = false;
+            this.BTNSortDesc.Click += new System.EventHandler(this.BTNSortDesc_Click);
+            // 
+            // DataListBox
+            // 
+            this.DataListBox.FormattingEnabled = true;
+            this.DataListBox.Location = new System.Drawing.Point(24, 302);
+            this.DataListBox.Name = "DataListBox";
+            this.DataListBox.Size = new System.Drawing.Size(305, 186);
+            this.DataListBox.TabIndex = 7;
+            this.DataListBox.SelectedIndexChanged += new System.EventHandler(this.DataListBox_SelectedIndexChanged);
+            // 
+            // DataSearch
+            // 
+            this.DataSearch.Location = new System.Drawing.Point(133, 121);
+            this.DataSearch.Name = "DataSearch";
+            this.DataSearch.Size = new System.Drawing.Size(196, 20);
+            this.DataSearch.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 286);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Click on a data point to edit above.";
+            // 
+            // GenerateRandomData
+            // 
+            this.GenerateRandomData.Location = new System.Drawing.Point(118, 4);
+            this.GenerateRandomData.Name = "GenerateRandomData";
+            this.GenerateRandomData.Size = new System.Drawing.Size(117, 23);
+            this.GenerateRandomData.TabIndex = 10;
+            this.GenerateRandomData.Text = "Generate Data";
+            this.GenerateRandomData.UseVisualStyleBackColor = true;
+            this.GenerateRandomData.Click += new System.EventHandler(this.GenerateRandomData_Click);
+            // 
+            // NeutrinoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 511);
+            this.Controls.Add(this.GenerateRandomData);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DataSearch);
+            this.Controls.Add(this.DataListBox);
+            this.Controls.Add(this.BTNSortDesc);
+            this.Controls.Add(this.BTNSortAsc);
+            this.Controls.Add(this.BTNSearch);
+            this.Controls.Add(this.BTNEditData);
+            this.Controls.Add(this.DataEditInput);
+            this.Controls.Add(this.BTNAddData);
+            this.Controls.Add(this.DataInput);
+            this.Name = "NeutrinoForm";
+            this.Text = "Neutrino Data Processing App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox DataInput;
+        private System.Windows.Forms.Button BTNAddData;
+        private System.Windows.Forms.TextBox DataEditInput;
+        private System.Windows.Forms.Button BTNEditData;
+        private System.Windows.Forms.Button BTNSearch;
+        private System.Windows.Forms.Button BTNSortAsc;
+        private System.Windows.Forms.Button BTNSortDesc;
+        private System.Windows.Forms.ListBox DataListBox;
+        private System.Windows.Forms.TextBox DataSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button GenerateRandomData;
+    }
+}
+
---
+++ SprintTwo/Form1.cs
@@ -0,0 +1,235 @@
+﻿// Nathan Booth, Sprint One, Neutrino
+// Date: 20/05/2025
+// Version: 1.01
+// Neutrino Data Form
+// Program to generate neutrino obersvations and sort them in ascending or descending order.
+// This program allows the user to add, edit, sort, and search for data points in a list of neutrino observations.
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace SprintOne
+{
+    public partial class NeutrinoForm : Form
+    {
+
+
+        public NeutrinoForm()
+        {
+            InitializeComponent();
+        }
+                
+        private int[] dataSet = new int[24];
+        private int currentIndex = 0;
+
+        private void BTNAddData_Click(object sender, EventArgs e)
+        {
+            int num1 = 0;
+
+            if (!Int32.TryParse(DataInput.Text, out num1))
+            {
+                MessageBox.Show("Please enter an integer.");
+                DataInput.Clear();
+                return;
+            }
+
+            if (currentIndex >= dataSet.Length)
+            {
+                MessageBox.Show("Maximum of 24 entries reached.");
+                DataInput.Clear();
+                return;
+            }
+
+            dataSet[currentIndex] = num1;
+            currentIndex++;
+
+            DataInput.Clear();
+            DataInput.Focus();
+
+            // Refresh the list box to show current data
+            DataListBox.Items.Clear();
+            for (int i = 0; i < currentIndex; i++)
+            {
+                DataListBox.Items.Add(dataSet[i]);
+            }
+        }
+
+
+        private void DataListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //display the selected index of list box in dataEditInput
+            if (DataListBox.SelectedIndex != -1)
+            {
+                DataEditInput.Text = DataListBox.SelectedItem.ToString();
+            }
+
+        }
+
+        private void BTNEditData_Click(object sender, EventArgs e)
+        {
+            //function to edit the data point from the array at the selected index
+            int selectedIndex = DataListBox.SelectedIndex;
+            if (selectedIndex != -1)
+            {
+                int newValue;
+                if (Int32.TryParse(DataEditInput.Text, out newValue))
+                {
+                    DataListBox.Items[selectedIndex] = newValue;
+                    DataEditInput.Clear();
+                    DataInput.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("Please enter an integer.");
+                    DataEditInput.Clear();
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a data point to edit.");
+            }
+        }
+
+        private void BTNSortAsc_Click(object sender, EventArgs e)
+        {
+            int temp = 0;
+            
+            //bubble sort the dataSet array onclick
+            for (int i = 0; i < currentIndex - 1; i++)
+            {
+                for (int j = 0; j < currentIndex - i - 1; j++)
+                {
+                    if (dataSet[j] > dataSet[j + 1])
+                    {
+                        temp = dataSet[j];
+                        dataSet[j] = dataSet[j + 1];
+                        dataSet[j + 1] = temp;
+                    }
+                }
+            }
+            //clear the list box and add the sorted data
+            DataListBox.Items.Clear();
+            for (int i = 0; i < currentIndex; i++)
+            {
+                DataListBox.Items.Add(dataSet[i]);
+            }
+        }
+
+        private void BTNSortDesc_Click(object sender, EventArgs e)
+        {
+            int temp = 0;
+
+            //bubble sort the dataSet array onclick
+            for (int i = 0; i < currentIndex - 1; i++)
+            {
+                for (int j = 0; j < currentIndex - i - 1; j++)
+                {
+                    if (dataSet[j] < dataSet[j + 1])
+                    {
+                        temp = dataSet[j];
+                        dataSet[j] = dataSet[j + 1];
+                        dataSet[j + 1] = temp;
+                    }
+                }
+            }
+            //clear the list box and add the sorted data
+            DataListBox.Items.Clear();
+            for (int i = 0; i < currentIndex; i++)
+            {
+                DataListBox.Items.Add(dataSet[i]);
+            }
+        }
+
+        private void GenerateRandomData_Click(object sender, EventArgs e)
+        {
+            //function to generate and fill the dataSet with 24 random integers
+            Random random = new Random();
+            for (int i = 0; i < dataSet.Length; i++)
+            {
+                dataSet[i] = random.Next(10, 90); // Random number between 1 and 100
+                currentIndex++;
+            }
+
+            //display the dataSet in the list box
+            DataListBox.Items.Clear();
+            for (int i = 0; i < dataSet.Length; i++)
+            {
+                DataListBox.Items.Add(dataSet[i]);
+            }
+            //clear the form
+            DataSearch.Clear();
+            DataInput.Clear();
+            DataEditInput.Clear();
+        }
+
+        private void BTNSearch_Click(object sender, EventArgs e)
+        {
+            //function to search for a value in the dataSet array, sorting items asc first.
+            int temp = 0;
+
+            //bubble sort the dataSet array onclick
+            for (int i = 0; i < currentIndex - 1; i++)
+            {
+                for (int j = 0; j < currentIndex - i - 1; j++)
+                {
+                    if (dataSet[j] > dataSet[j + 1])
+                    {
+                        temp = dataSet[j];
+                        dataSet[j] = dataSet[j + 1];
+                        dataSet[j + 1] = temp;
+                    }
+                }
+            }
+            //clear the list box and add the sorted data
+            DataListBox.Items.Clear();
+            for (int i = 0; i < currentIndex; i++)
+            {
+                DataListBox.Items.Add(dataSet[i]);
+            }
+            //binary search function
+            int searchValue;
+            if (Int32.TryParse(DataSearch.Text, out searchValue))
+            {
+                int left = 0;
+                int right = currentIndex - 1;
+                bool found = false;
+                while (left <= right)
+                {
+                    int mid = left + (right - left) / 2;
+                    if (dataSet[mid] == searchValue)
+                    {
+                        MessageBox.Show("Value found at index: " + mid);
+                        found = true;
+                        break;
+                    }
+                    else if (dataSet[mid] < searchValue)
+                    {
+                        left = mid + 1;
+                    }
+                    else
+                    {
+                        right = mid - 1;
+                    }
+                }
+                if (!found)
+                {
+                    MessageBox.Show("Value not found.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter an integer.");
+            }
+        }
+    }
+}
